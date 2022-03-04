b627d4442ff5755a8cd06d86c9c87c5544f3f41c
Elhassan7 <chakouki.el.hassan@gmail.com>
2022-03-04 22:12:10 +0100

    git push

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ st.set_page_config(page_title= "Sales Dashboard",
                     layout='wide')
 
 df= pd.read_csv("supermarkt_sales.csv")
-st.dataframe(df)
 
 #-----------SideBar-------------
 st.sidebar.header("Filtering by :")
@@ -15,6 +14,15 @@ city= st.sidebar.multiselect("Select The City :",
                         options=df["City"].unique(),
                         default=["Yangon"] )
 
-customer_type= st.sidebar.multiselect("Select The City :",     
+customer_type= st.sidebar.multiselect("Select type of customer :",     
                         options=df["Customer_type"].unique(),
                         default=df["Customer_type"].unique())
+
+Gender= st.sidebar.multiselect("Select The Gender :",     
+                        options=df["Gender"].unique(),
+                        default=df["Gender"].unique())
+
+
+df_city= df.query("City== @city & Customer_type==@customer_type & Gender==@Gender")
+
+st.dataframe(df_city)
