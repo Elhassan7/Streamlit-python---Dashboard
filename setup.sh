465837dd39118dc5b6272ac3c62fb2fc6cd708e5
Elhassan7 <chakouki.el.hassan@gmail.com>
2022-03-07 14:36:54 +0100

    git push

--- setup.sh
+++ setup.sh
@@ -3,11 +3,11 @@ mkdir -p ~/.streamlit/
 echo "\
 [general]\n\
 email = \"chakouki.el.hassan@gmail.com\"\n\
-" > ~/.streamlit/config.toml
+" > ./.streamlit/config.toml
 
 echo "\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
-" > ~/.streamlit/config.toml
+" > ./.streamlit/config.toml
